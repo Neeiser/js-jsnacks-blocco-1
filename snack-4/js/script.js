41a1a9db9d3f2099c10d5ea1ffc53373f1f2b115
Neeiser <stortini.study@gmail.com>
2022-02-25 15:21:09 +0100

    Snack 4

---
+++ snack-4/js/script.js
@@ -0,0 +1,21 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. */
+/* Chiedi all’utente il suo nome. */
+/* Comunicagli se può partecipare o no alla festa. */
+
+const arrInvitedGuests = ['Agamennone', 'Achille', 'Ettore', 'Menelao', 'Rodrigo Borgia', 'Putin',];
+
+let guestNameInput = prompt('Il messere favorisca il nome prego:').toUpperCase;
+let match= false;
+
+for (let arrItem = 0; arrItem < arrInvitedGuests.length; arrItem++){
+
+    if (arrInvitedGuests[arrItem].toUpperCase == guestNameInput) {
+        match= true;
+    }
+}
+
+if (match == true) {
+    alert('Può entrare messere!');
+} else {
+    alert('Lei è un ciarlatano! Guardie, allontanate questo impostore!');
+}
