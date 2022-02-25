0e4de943374f61d565837d2ffc56e926503963a6
Neeiser <stortini.study@gmail.com>
2022-02-25 16:43:22 +0100

    Snack 6 bugged

---
+++ snack-6/js/script.js
@@ -0,0 +1,12 @@
+
+/* Chiedi un numero di 4 cifre all’utente */
+/* Calcola la somma di tutte le cifre che compongono il numero. */
+
+let userNumber= parseInt(prompt('Scrivi un numero positivo di 4 cifre'));
+let numberArrey= [];
+
+
+
+
+
+
