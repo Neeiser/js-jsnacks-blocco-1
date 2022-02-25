427391d4b51921cd27172db24ec3c34f810609dd
Neeiser <stortini.study@gmail.com>
2022-02-25 18:22:13 +0100

    Snack 6

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -2,9 +2,16 @@
 /* Chiedi un numero di 4 cifre all’utente */
 /* Calcola la somma di tutte le cifre che compongono il numero. */
 
-let userNumber= parseInt(prompt('Scrivi un numero positivo di 4 cifre'));
-let numberArrey= [];
+const inputNumeroInserito= prompt('Scrivi un numero positivo di 4 cifre');
 
+let somma=0
+
+for (let i = 0; i < inputNumeroInserito.length; i++) {
+
+    somma += parseInt(inputNumeroInserito[i]); 
+}
+
+alert(somma);
 
 
 
