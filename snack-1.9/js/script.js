1600cf85b6834cb6c5735d39fd07fd8d436087eb
Neeiser <stortini.study@gmail.com>
2022-02-28 15:12:28 +0100

    Snack 1.9

--- snack-1.9/js/script.js
+++ snack-1.9/js/script.js
@@ -19,12 +19,23 @@ console.log('la somma totale è di: ' + sum);
 console.log('la media totale è di: ' + media); 
 
 
-/* 
-const whileArrNumbers = [];
+/* while cycle */
+
+const whileArrNumeri = [];
 let whileSum = 0;
 let whileMedia = 0;
 
-while () {
+let index= 0;
 
-} 
-*/
+while (index < 10) {
+    whileArrNumeri.push(index + 1);
+    whileSum += whileArrNumeri[index];
+    index++;
+    console.log(index);
+
+}
+
+whileMedia = whileSum / 10;
+
+console.log('la somma totale è di: ' + whileSum);
+console.log('la media totale è di: ' + whileMedia);
